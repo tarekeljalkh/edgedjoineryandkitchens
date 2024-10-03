d16f482101aa793029929c0509c98d2c8ea9aa90
Tarek <tarekeljalkh@gmail.com>
2024-10-03 21:21:30 +0300

    web storage link

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@ Route::resource('/', FrontendController::class);
 Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
 Route::resource('/gallery', GalleryController::class);
 Route::post('/newsletter-subscribe', [NewsletterController::class, 'subscribe'])->name('newsletter.subscribe');
+
+
+// Route::get('/storage-link', function () {
+//     // Full path to your storage folder
+//     $target = '/home/u236778855/domains/edgedjoinerykitchens.com.au/edgedjoinerykitchens/storage/app/public';
+
+//     // Full path to the location where you want the symlink (public_html)
+//     $link = '/home/u236778855/domains/edgedjoinerykitchens.com.au/public_html/storage';
+
+//     // Create the symlink
+//     symlink($target, $link);
+
+//     return 'Storage link has been created successfully';
+// });
+
+// Route::get('/check-path', function () {
+//     return base_path();
+// });
